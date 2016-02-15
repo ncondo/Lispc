8af9679b63e427ce2a1f9397197bb684dd829aa1
Nick <nicholas.condo@gmail.com>
2016-02-15 11:38:19 -0500

    Implement evaluation of parse tree (AST)

--- prompt.c
+++ prompt.c
@@ -28,6 +28,36 @@ void add_history(char* unused) {}
 #include <editline/readline.h>
 #endif
 
+/* Use operator string to see which operation to perform */
+long eval_op(long x, char* op, long y) {
+    if (strcmp(op, "+") == 0) { return x + y; }
+    if (strcmp(op, "-") == 0) { return x - y; }
+    if (strcmp(op, "*") == 0) { return x * y; }
+    if (strcmp(op, "/") == 0) { return x / y; }
+    return 0;
+}
+
+long eval(mpc_ast_t* t) {
+    /* If tagged as number return it directly */
+    if (strstr(t->tag, "number")) {
+        return atoi(t->contents);
+    }
+
+    /* The operator is always second child */
+    char* op = t->children[1]->contents;
+
+    /* Store third child in 'x' */
+    long x = eval(t->children[2]);
+
+    /* Iterate through remaining children and combine */
+    int i = 3;
+    while (strstr(t->children[i]->tag, "expr")) {
+        x = eval_op(x, op, eval(t->children[i]));
+        i++;
+    }
+    return x;
+}
+
 
 int main(int argc, char** argv) {
 
@@ -48,7 +78,7 @@ int main(int argc, char** argv) {
         Number, Operator, Expr, Lispc);
 
     /* Print version and exit information */
-    puts("Lispc Version 0.0.1");
+    puts("Lispc Version 0.0.3");
     puts("Press Ctrl+c to Exit\n");
 
     /* In a never ending loop */
@@ -63,8 +93,8 @@ int main(int argc, char** argv) {
         /* Parse the user input */
         mpc_result_t r;
         if (mpc_parse("<stdin>", input, Lispc, &r)) {
-            /* On success print the AST */
-            mpc_ast_print(r.output);
+            long result = eval(r.output);
+            printf("%li\n", result);
             mpc_ast_delete(r.output);
         } else {
             /* Otherwise print the error */
