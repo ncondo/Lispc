2db9d9c4da566e02109d4838fb2c1f121d352fb5
Nick <nicholas.condo@gmail.com>
2016-02-15 01:58:05 -0500

    Implement basic parser using mpc library

--- prompt.c
+++ prompt.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#include "mpc.h"
+
 /* If we are compiling on Windows use these functions */
 #ifdef _WIN32
 #include <string.h>
@@ -29,6 +31,22 @@ void add_history(char* unused) {}
 
 int main(int argc, char** argv) {
 
+    /* Create Parsers */
+    mpc_parser_t* Number    = mpc_new("number");
+    mpc_parser_t* Operator  = mpc_new("operator");
+    mpc_parser_t* Expr      = mpc_new("expr");
+    mpc_parser_t* Lispc     = mpc_new("lispc");
+
+    /* Define parsers with the following Language */
+    mpca_lang(MPCA_LANG_DEFAULT,
+        "                                                       \
+            number   : /-?[0-9]+/ ;                             \
+            operator : '+' | '-' | '*' | '/' ;                  \
+            expr     : <number> | '(' <operator> <expr>+ ')' ;  \
+            lispc    : /^/ <operator> <expr>+ /$/ ;             \
+        ",
+        Number, Operator, Expr, Lispc);
+
     /* Print version and exit information */
     puts("Lispc Version 0.0.1");
     puts("Press Ctrl+c to Exit\n");
@@ -42,12 +60,23 @@ int main(int argc, char** argv) {
         /* Add input to history */
         add_history(input);
 
-        /* Echo input back to user */
-        printf("=> %s\n", input);
+        /* Parse the user input */
+        mpc_result_t r;
+        if (mpc_parse("<stdin>", input, Lispc, &r)) {
+            /* On success print the AST */
+            mpc_ast_print(r.output);
+            mpc_ast_delete(r.output);
+        } else {
+            /* Otherwise print the error */
+            mpc_err_print(r.error);
+            mpc_err_delete(r.error);
+        }
 
         /* Free retrieved input */
         free(input);
     }
+
+    mpc_cleanup(4, Number, Operator, Expr, Lispc);
 
     return 0;
 }
