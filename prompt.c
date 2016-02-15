4375df8a38d641dd6745d635121c7331e8f5bbb9
Nick <nicholas.condo@gmail.com>
2016-02-14 21:50:08 -0500

    Add Windows compatibility using faux editline functions

--- prompt.c
+++ prompt.c
@@ -1,7 +1,30 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/* If we are compiling on Windows use these functions */
+#ifdef _WIN32
+#include <string.h>
+
+static char buffer[2048];
+
+/* Faux readline function for Windows compatibility */
+char* readline(char* prompt) {
+    fputs(prompt, stdout);
+    fgets(buffer, 2048, stdin);
+    char* cpy = malloc(strlen(buffer)+1);
+    strcpy(cpy, buffer);
+    cpy[strlen(cpy)-1] = '\0';
+    return cpy;
+}
+
+/* Faux add_history function for Windows compatibility */
+void add_history(char* unused) {}
+
+
+/* Otherwise, include editline headers for Linux/Mac */
+#else
 #include <editline/readline.h>
+#endif
 
 
 int main(int argc, char** argv) {
