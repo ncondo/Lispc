8ced67fb0271175d5780a1293869128e109808e7
Nick <nicholas.condo@gmail.com>
2016-02-14 21:37:26 -0500

    Add history functionality using up/down arrows

--- prompt.c
+++ prompt.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-/* Declare a buffer for user input of size 2048 */
-static char input[2048];
+#include <editline/readline.h>
+
 
 int main(int argc, char** argv) {
 
@@ -12,14 +13,17 @@ int main(int argc, char** argv) {
     /* In a never ending loop */
     while(1) {
 
-        /* Output our prompt */
-        fputs("lispc> ", stdout);
+        /* Output our prompt and get input */
+        char* input = readline("lispc> ");
 
-        /* Read a line of user input of max size 2048 */
-        fgets(input, 2048, stdin);
+        /* Add input to history */
+        add_history(input);
 
         /* Echo input back to user */
-        printf("=> %s", input);
+        printf("=> %s\n", input);
+
+        /* Free retrieved input */
+        free(input);
     }
 
     return 0;
